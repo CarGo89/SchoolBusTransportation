7e83725dc2a84604744043a53d478f7471fba294
Carlos González <karl0s_alberto0@hotmail.com>
2016-10-06 17:46:03 -0500

    Added the navigation-controller.

---
+++ SchoolBusWeb/Scripts/controllers/navigation-controller.js
@@ -0,0 +1,13 @@
+﻿(function (angular) {
+    "use strict";
+
+    var schoolBus = angular.module("schoolBus", ["ngAnimate"]);
+
+    schoolBus.controller("navigationController", ["$scope", function ($scope) {
+        $scope.editMode = false;
+
+        $scope.setEditMode = function () {
+            $scope.editMode = !$scope.editMode;
+        };
+    }]);
+})(window.angular);
