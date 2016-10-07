276b66be44508c3f2168af985080452be653f43f
Carlos González <karl0s_alberto0@hotmail.com>
2016-10-07 12:42:46 -0500

    Added the jquery.DataTable component.

---
+++ SchoolBusWeb/Scripts/controllers/driver-controller.js
@@ -0,0 +1,46 @@
+﻿(function (angular) {
+    "use strict";
+
+    var schoolBus = angular.module("schoolBus", ["ngAnimate"]);
+
+    schoolBus.directive("responsiveDataTable", window.angularDirectives.responsiveDataTable);
+
+    schoolBus.directive("navigationContainer", window.angularDirectives.navigationContainer);
+
+    schoolBus.controller("driverController", ["$scope", function ($scope) {
+        $scope.dataTableLanguage = {
+            search: "Filtrar:",
+            searchPlaceholder: "Filtrar Choferes",
+            emptyTable: "Sin Resultados",
+            zeroRecords: "Sin Resultados",
+            lengthMenu: "Mostrar _MENU_ Choferes",
+            info: "Mostrando _START_ to _END_ of _TOTAL_ Choferes",
+            infoEmpty: "Mostrando 0 a 0 de 0 Choferes",
+            infoFiltered: "(Choferes filtrados de _MAX_)"
+        };
+
+        $scope.data = [
+        {
+            Name: "Carlos",
+            MiddleName: "Alberto",
+            LastName: "Gonzalez",
+            SecondLastName: "Morales",
+            BirthDate: "05/09/1989"
+        }];
+
+        $scope.drivers = $scope.data;
+        $scope.driverToUpdate = {};
+
+        $scope.edit = function(driver) {
+            $scope.driverToUpdate = driver;
+
+            $scope.setEditMode();
+        };
+
+        $scope.cancelEdit = function () {
+            $scope.driverToUpdate = {};
+
+            $scope.setReadMode();
+        };
+    }]);
+})(window.angular);
---
+++ SchoolBusWeb/Scripts/utilities/angular-directives.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("schoolBus", []);
+
+    window.angularDirectives = {
+        navigationContainer: function () {
+            return {
+                restrict: "A",
+
+                link: function (scope) {
+                    scope.pageMode = "read";
+
+                    scope.setReadMode = function () {
+                        scope.pageMode = "read";
+                    };
+
+                    scope.setEditMode = function () {
+                        scope.pageMode = "edit";
+                    };
+                }
+            }
+        },
+
+        responsiveDataTable: function () {
+            var initDataTable = function (scope, element) {
+                element.dataTable({
+                    destroy: true,
+                    responsive: true,
+                    autoWidth: true,
+                    deferRender: true,
+                    sort: true,
+                    order: [[0, "desc"]],
+                    language: scope.dataTableLanguage
+                });
+            };
+
+            return {
+                restrict: "A",
+
+                link: function (scope, element) {
+                    scope.initDataTable = function (render) {
+                        if (render === true) {
+                            initDataTable(scope, element);
+                        }
+                    };
+
+                    if (scope.data.length === 0) {
+                        initDataTable(scope, element);
+                    }
+                }
+            };
+        }
+    };
+})();
